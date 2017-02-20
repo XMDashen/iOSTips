bede871b354628de1e716c4fe52de89ad428efa1
xiexi <18380461374@163.com>
2017-02-20 16:39:02 +0800

    添加计算高度

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ require('UIAlertView')
 
 defineClass("XMHomeViewController", {
     shutDownAppIfNeeded: function() {
-    	var av = UIAlertView.alloc().initWithTitle_message_delegate_cancelButtonTitle_otherButtonTitles("警告", "如果看到此窗口,请马上联系开发者,您现在正在使用盗版软件!!!", null, "取消", "确定", null);
+    	var av = UIAlertView.alloc().initWithTitle_message_delegate_cancelButtonTitle_otherButtonTitles("警告", "非法使用,请尽快联系开发者!!!", null, "取消", "确定", null);
             av.show();
         var alertVc = UIAlertController.alertControllerWithTitle_message_preferredStyle("警告", "非法使用", 1);
         self.presentViewController_animated_completion(alertVc, YES, null);
